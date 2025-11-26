362e3b8015135547eb313eeca1ea90de986e3c04
shaan-hossain-01 <rockshaan200@gmail.com>
2025-11-26 20:55:04 +0600

    started from scratch

--- backend/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-import env from './config/env';
-
-app.listen(env.PORT, () => {
-  console.log(`API is listening on http://${env.HOST}:${env.PORT} (stage=${env.APP_STAGE})`);
-})
